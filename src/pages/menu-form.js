ca6cfa9a3b1bc09f6fe90ff706a407eb82b69741
Adam Bowman <adamcbowman@gmail.com>
2022-05-06 08:41:05 -0300

    add coffee/desert/flatware

--- src/pages/menu-form.js
+++ src/pages/menu-form.js
@@ -2,11 +2,6 @@ import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { useState } from "react"
-// import PlacesAutocomplete, {
-//   geocodeByAddress,
-//   getLatLng,
-// } from 'react-places-autocomplete';
-// import {Helmet} from "react-helmet";
 
 const MenuForm = () => {
     
@@ -21,6 +16,11 @@ const MenuForm = () => {
   const [chicken, setChicken] = React.useState(0)
   const [chickenPork, setChickenPork] = React.useState(0)
   const [pigRoast, setPigRoast] = React.useState(0)
+
+  const [desertCharge, setDesertCharge] = React.useState(0)
+  const [coffeeCharge, setCoffeeCharge] = React.useState(0)
+  const [flatwareCharge, setFlatwareCharge] = React.useState(0)
+  const [flatware, setFlatware] = React.useState("")
 
   const [address, setAddress] = useState("")
  
@@ -51,6 +51,17 @@ const MenuForm = () => {
         : setPigRoast((e.target.value * 28.45).toFixed(2))
       : setPigRoast(0)
   }
+  const handleDesert = e => {
+    e.target.value === "Both" ? setDesertCharge(groupSize*.75) : setDesertCharge(0)
+  }
+  const handleCoffee = e => {
+    e.target.value === "Coffee" ? setCoffeeCharge(groupSize*.5) : setCoffeeCharge(0)
+  }
+
+  const handleFlatware = e => {
+    setFlatware(e.target.value)
+    setFlatwareCharge(0)
+  }
 
   const handleSubmit = e => {
     e.preventDefault()
@@ -77,19 +88,7 @@ const MenuForm = () => {
   const handleLocation = e => {
     setAddress(e.target.value)
   }
-
-  // const handlePlaceChange = address => {
-  //   setAddress(address)
-  // }
-
-  // const handlePlaceSelect = address => {
-  //   geocodeByAddress(address)
-  //     .then(results => getLatLng(results[0]))
-  //     .then(latLng => console.log("Success", latLng))
-  //     .catch(error => console.error("Error", error))
-  // }
   
-
   useEffect(() => {
     setPrice(
       (
@@ -97,10 +96,14 @@ const MenuForm = () => {
         parseFloat(chicken) +
         parseFloat(chickenPork) +
         parseFloat(pigRoast) + 
+        parseFloat(desertCharge) +
+        parseFloat(coffeeCharge) +
+        parseFloat(flatwareCharge)
+
         (groupSize && groupSize > 0 ? parseFloat(setupFee) : 0)
       ).toFixed(2)
     )
-  }, [steak, chicken, chickenPork, pigRoast, setupFee, groupSize])
+  }, [steak, chicken, chickenPork, pigRoast, setupFee, desertCharge, coffeeCharge, flatwareCharge, groupSize])
 
   useEffect(() => {
     setSetupFee(
@@ -120,6 +123,7 @@ const MenuForm = () => {
 
       </div>
       <div className="flex flex-col items-left">
+        {/* group siz */}
         <div className="flex justify-between border">
           <label htmlFor="group-size" className="p-2">
             Group Size - Adults
@@ -149,7 +153,7 @@ const MenuForm = () => {
             onChange={handleInfants}
           />
         </div>
-
+        {/* chicken/pork */}
         <div className="flex items-center border justify-between">
           <label htmlFor="chickenPork" className="p-2">
             Chicken(60%) / Pork(40%) @ $17
@@ -163,7 +167,7 @@ const MenuForm = () => {
           />
           <p className="p-2">${chickenPork}</p>
         </div>
-
+        {/* pork */}
         <div className="flex items-center border w-full justify-between">
           <label htmlFor="steak" className="p-2">
             Boneless Pork Steak @ $16.45
@@ -177,7 +181,7 @@ const MenuForm = () => {
           />
           <p className="p-2">${steak}</p>
         </div>
-
+        {/* chicken */}
         <div className="flex items-center border justify-between">
           <label htmlFor="chicken" className="p-2">
             Chicken Breast (6 oz, boneless) @ $17.75
@@ -191,7 +195,7 @@ const MenuForm = () => {
           />
           <p className="p-2">${chicken}</p>
         </div>
-
+        {/* pig roast */}
         <div className="flex items-center pt-2 border justify-between">
           <div>
             <label htmlFor="pig-roast" className="p-2">
@@ -211,17 +215,75 @@ const MenuForm = () => {
           />
           <p className="p-2">${pigRoast}</p>
         </div>
+        {/* desert */}
+        <div className="flex items-center border justify-between">
+          <label htmlFor="desert" className="p-2">
+            Deserts
+          </label>
+          <select
+            type="select"
+            id="desert"
+            className="w-6/12"
+            onChange={handleDesert}
+          >
+            <option value="">Select</option>
+            <option value="Carrot Cake">Carrot Cake</option>
+            <option value="Apple Crisp">Apple Crisp</option>
+            <option value="Both">Both (+$0.75/person)</option>
+          </select>
+        </div>
+        {/* coffee */}
+        <div className="flex items-center border justify-between">
+          <label htmlFor="coffee" className="p-2">
+            Coffee/Tea
+          </label>
+          <select
+            type="select"
+            id="coffee"
+            className="w-6/12"
+            onChange={handleCoffee}
+          >
+            <option value="">Select</option>
+            <option value="Coffee">Coffee/Tea (+$0.50/person)</option>
+            <option value="No">No</option>
+          </select>
+        </div>
 
+        {/* flatware */}
+        <div>
+        <div className="flex items-center border justify-between">
+          <label htmlFor="flatware" className="p-2">
+            Flatware
+          </label>
+          <select
+            type="select"
+            id="flatware"
+            className="w-6/12"
+            onChange={handleFlatware}
+          >
+            <option value="">Select</option>
+            <option value="Royal">RoyalChinette - disposable</option>
+            <option value="Rental"> Flatware Rental (+ $1.75/person & 15% clearing charge)</option>
+            <option value="Upgrade"> Flatware Upgrade (+ $3/person)</option>
+          </select>
+        </div>
+        <p className="text-xs">
+          { flatware === 'Royal' ? 'RoyalChinette - included in price - PLATE, FORK, KNIFE, SPOON - disposable' : '' }
+          { flatware === 'Rental' ? 'Flatware Rental - Stoneware and silverware - Plate, Fork, Knife, Spoon, $1.75 per setting plus 15% clearing charge' : '' }
+          { flatware === 'Upgrade' ? 'Disposable Flatware upgrade - real lookbut disposable - PLATE, FORK, KNIFE, SPOON -  $3/person, no clearning charge' : '' }
+        </p>
+        </div>
+
+        {/* setup fee */}
         {groupSize > 0 && groupSize < 100 ? (
           <>
           <div className="flex items-center justify-between p-2 ">
             <p>Set Up Charge:</p> <p>${setupFee}</p>
           </div>
           <p className="text-xs p-2">Applies to groups under 100</p>
-          </>
-          
+          </>  
         ) : null}
-
+        {/* location */}
         <div className="flex items-center p-2">
           <label htmlFor="location" className="p-2">
             Location
@@ -234,50 +296,7 @@ const MenuForm = () => {
             placeholder="Enter Event Location Address"
           />
         </div>
-
-        {/* <div className="flex items-center p-2">
-          <PlacesAutocomplete
-            value={address}
-            onChange={handlePlaceChange}
-            onSelect={handlePlaceSelect}
-          >
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-              <div>
-                <input
-
-                  {...getInputProps({
-                    placeholder: "Search Places ...",
-                    className: "location-search-input"
-                  })}
-                />
-                <div className="autocomplete-dropdown-container">
-                  {loading && <div>Loading...</div>}
-                  {suggestions.map(suggestion => {
-                    const className = suggestion.active
-
-                      ? "suggestion-item--active"
-                      : "suggestion-item";
-                    // inline style for demonstration purpose 
-                    const style = suggestion.active
-                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
-                      : { backgroundColor: "#ffffff", cursor: "pointer" };
-                    return (
-                      <div
-                        {...getSuggestionItemProps(suggestion, {
-                          className,
-                          style
-                        })}
-                      >
-                        <span>{suggestion.description}</span>
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            )}
-          </PlacesAutocomplete>
-        </div> */}
-
+        {/* total */}
         <div className="flex items-center border justify-between">
           <label htmlFor="totalCost" className="p-2">
             Total Cost
@@ -286,6 +305,7 @@ const MenuForm = () => {
         </div>
         <button type="button" className="btn btn-md btn-primary" onClick={handleSubmit}>Continue to Service Outline Form</button>
       </div>
+
     </Layout>
   )
 }
