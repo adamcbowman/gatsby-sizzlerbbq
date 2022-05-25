bdc508bde1ea9c7c1b0658c130b11b0c348e12a8
Adam Bowman <adamcbowman@gmail.com>
2022-05-25 18:23:31 -0300

    add menu items

--- src/pages/menu-form.js
+++ src/pages/menu-form.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react"
 import MapboxAutocomplete from "react-mapbox-autocomplete"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhone } from "@fortawesome/free-solid-svg-icons"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -25,6 +27,14 @@ const MenuForm = () => {
   const [pigRoastQty, setPigRoastQty] = React.useState(0)
   const [steak, setSteak] = React.useState(0)
   const [steakQty, setSteakQty] = React.useState(0)
+  const [salmon, setSalmon] = React.useState(0)
+  const [salmonQty, setSalmonQty] = React.useState(0)
+  const [vegKabob, setVegKabob] = React.useState(0)
+  const [vegKabobQty, setVegKabobQty] = React.useState(0)
+  const [lobster, setLobster] = React.useState(0)
+  const [lobsterQty, setLobsterQty] = React.useState(0)
+  const [roastBeef, setRoastBeef] = React.useState(0)
+  const [roastBeefQty, setRoastBeefQty] = React.useState(0)
 
   const [desertCharge, setDesertCharge] = React.useState(0)
   const [desert, setDesert] = React.useState("")
@@ -75,11 +85,45 @@ const MenuForm = () => {
     setSteak((e.target.value * 23.95).toFixed(2))
   }
   const handleSteakMushrooms = e => {
-    e.target.checked ? setSteak((parseInt(steak) + steakQty * 3.25).toFixed(2)) : setSteak((parseInt(steak) - (steakQty * 3.25)).toFixed(2))
+    e.target.checked
+      ? setSteak((parseInt(steak) + steakQty * 3.25).toFixed(2))
+      : setSteak((parseInt(steak) - steakQty * 3.25).toFixed(2))
   }
   const handleSteakOnions = e => {
-    e.target.checked ? setSteak((parseInt(steak) + (steakQty * 1.75)).toFixed(2)) : setSteak((parseInt(steak) - (steakQty * 1.75)).toFixed(2))
+    e.target.checked
+      ? setSteak((parseInt(steak) + steakQty * 1.75).toFixed(2))
+      : setSteak((parseInt(steak) - steakQty * 1.75).toFixed(2))
   }
+  const handleSalmon = e => {
+    setSalmon((e.target.value * 23.45).toFixed(2))
+    setSalmonQty(e.target.value)
+  }
+  const handleVegKabob = e => {
+    setVegKabob((e.target.value * 16.45).toFixed(2))
+    setVegKabobQty(e.target.value)
+  }
+  const handleLobster = e => {
+    setLobster((e.target.value * 16.45).toFixed(2))
+    setLobsterQty(e.target.value)
+  }
+  const handleRoastBeef = e => {
+    setRoastBeef((e.target.value * 19.75).toFixed(2))
+    setRoastBeefQty(e.target.value)
+
+    setDesert("Both")
+    setDesertCharge(0)
+  }
+  const handleRoastBeefMushrooms = e => {
+    e.target.checked
+      ? setRoastBeef((parseInt(roastBeef) + roastBeefQty * 3.25).toFixed(2))
+      : setRoastBeef((parseInt(roastBeef) - roastBeefQty * 3.25).toFixed(2))
+  }
+  const handleRoastBeefOnions = e => {
+    e.target.checked
+      ? setRoastBeef((parseInt(roastBeef) + roastBeefQty * 1.75).toFixed(2))
+      : setRoastBeef((parseInt(roastBeef) - roastBeefQty * 1.75).toFixed(2))
+  }
+
   const handleDesert = e => {
     e.target.value === "Both"
       ? setDesertCharge(groupSize * 0.75)
@@ -107,19 +151,23 @@ const MenuForm = () => {
   }
 
   const getFormDetailsText = () => {
-    //todo: add quantities for menu items
-    let menu = `
-    Pork Steak: ${porkSteak},  
-    Steak: ${steak},
-    Chicken: ${chicken},  
-    Chicken/Pork: ${chickenPork},  
-    Pig Roast: ${pigRoast},  
-    Desert: ${desert} ${desertCharge},
-    Coffee: ${coffeeCharge},
-    Flatware: ${flatware} ${flatwareCharge}
-    Setup Fee: ${setupFee},  
-    Travel Fee: ${travelFee}},
-    Total: ${price}
+
+    let menu = 
+    `
+    Steak: ${steakQty} @$${steak},
+    Chicken: ${chickenQty} @$${chicken}, 
+    Salmon: ${salmonQty} @$${salmon},
+    Tofu Veg Kabob: ${vegKabobQty} @$${vegKabob}, 
+    Lobster: ${lobsterQty} @$${lobster},
+    Chicken/Pork: ${chickenPorkQty} @$${chickenPork},  
+    Roast Beef: ${roastBeefQty} @$${roastBeef},
+    Pig Roast: ${pigRoastQty} @$${pigRoast}, 
+    Desert: ${desert} @$${desertCharge},
+    Coffee: $${coffeeCharge},
+    Flatware: ${flatware} @$${flatwareCharge}
+    Setup Fee: $${setupFee},  
+    Travel Fee: $${travelFee},
+    Total: $${price}
       `
     return menu
   }
@@ -232,9 +280,18 @@ const MenuForm = () => {
       <div className="flex flex-col items-center p-6">
         <h1 className="text-2xl font-bold text-primary">Menu Selection Form</h1>
       </div>
+
+      <div className=" text-center pt-2">
+          <a href="tel:1-800-805-2227">
+            <FontAwesomeIcon icon={faPhone} />
+            {"  "}
+            Call for free Consultation
+          </a>
+        </div>
+
       <div className="flex flex-col items-left">
         {/* group size */}
-        <p className="p-2 text-xs">
+        <p className="p-2 pt-4 text-xs">
           Select the total number of guests in your group.
         </p>
         <div className="flex flex-col w-full md:flex-row items-center justify-items-center border">
@@ -273,8 +330,33 @@ const MenuForm = () => {
 
         <p className="p-2 text-xs">
           Select the number of servings for each menu choice below. Buffet
-          included with all menu options. <Link to="/menu">See menu</Link> for details
+          included with all menu options. <Link to="/menu">See menu</Link> for
+          details
         </p>
+
+        {/* chicken/pork */}
+        <div className="flex flex-col md:flex-row items-center border justify-between">
+          <div className="w-full md:w-1/3">
+            <label htmlFor="chickenPork" className="p-2 label">
+              Chicken(60%) / Pork(40%) @ $17
+            </label>
+            <p className="px-2 pt-2 text-xs font-semibold">
+              Most popular menu choice.
+            </p>
+            <p className="p-2 text-xs">
+              A combination of our 2 most popular items, Chicken Breast & Pork
+              Steak!
+            </p>
+          </div>
+          <input
+            type="number"
+            id="chickenPork"
+            className="px-2 input input-bordered input-primary"
+            onChange={handleChickenPork}
+            defaultValue={0}
+          />
+          <p className="p-2">${chickenPork}</p>
+        </div>
 
         {/* pork steak */}
         <div className="flex flex-col md:flex-row items-center border justify-between">
@@ -329,7 +411,11 @@ const MenuForm = () => {
               </label>
             </div>
             <div className="flex px-2">
-              <input type="checkbox" id="steak-onions" onChange={handleSteakOnions} />
+              <input
+                type="checkbox"
+                id="steak-onions"
+                onChange={handleSteakOnions}
+              />
               <label htmlFor="steak-onions" className="p-2 label text-xs">
                 Add onions @ $1.75
               </label>
@@ -345,22 +431,62 @@ const MenuForm = () => {
           <p className="p-2">${steak}</p>
         </div>
 
-        {/* chicken/pork */}
+        {/* Salmon */}
         <div className="flex flex-col md:flex-row items-center border justify-between">
           <div className="w-full md:w-1/3">
-            <label htmlFor="chickenPork" className="p-2 label">
-              Chicken(60%) / Pork(40%) @ $17
+            <label htmlFor="salmon" className="p-2 label">
+              Salmon @ $23.45
             </label>
-            <p className="p-2 text-xs">Most popular menu choice.</p>
+            <p className="p-2 text-xs"></p>
           </div>
           <input
             type="number"
-            id="chickenPork"
+            id="salmon"
             className="p-2 input input-bordered input-primary"
-            onChange={handleChickenPork}
+            onChange={handleSalmon}
             defaultValue={0}
           />
-          <p className="p-2">${chickenPork}</p>
+          <p className="p-2">${salmon}</p>
+        </div>
+
+        {/* Tofu Veggie Kabob  */}
+        <div className="flex flex-col md:flex-row items-center border justify-between">
+          <div className="w-full md:w-1/3">
+            <label htmlFor="vegKabob" className="p-2 label">
+              Tofu Veggie Kabob @ $16.45
+            </label>
+            <p className="p-2 text-xs">Available without tofu on request. </p>
+          </div>
+          <input
+            type="number"
+            id="vegKabob"
+            className="p-2 input input-bordered input-primary"
+            onChange={handleVegKabob}
+            defaultValue={0}
+          />
+          <p className="p-2">${vegKabob}</p>
+        </div>
+
+        {/* Lobster  */}
+
+        <div className="flex flex-col md:flex-row items-center border justify-between">
+          <div className="w-full md:w-1/3">
+            <label htmlFor="lobster" className="p-2 label">
+              Lobster @ $16.45
+            </label>
+            <p className="p-2 text-xs">
+              {" "}
+              + Lobster cost (you set the size and quantity){" "}
+            </p>
+          </div>
+          <input
+            type="number"
+            id="lobster"
+            className="p-2 input input-bordered input-primary"
+            onChange={handleLobster}
+            defaultValue={0}
+          />
+          <p className="p-2">${lobster}</p>
         </div>
 
         {/* pig roast */}
@@ -383,6 +509,52 @@ const MenuForm = () => {
           />
           <p className="p-2">${pigRoast}</p>
         </div>
+
+        {/* Unlimited Hip of Beef  */}
+        <div className="flex flex-col md:flex-row items-center border justify-between">
+          <div className="w-full md:w-1/3">
+            <label htmlFor="roastBeef" className="p-2 label">
+              Unlimited Hip of Beef @ $19.75
+            </label>
+            <br />
+            <p className="p-2 text-xs">Includes both desserts</p>
+            <div className="flex px-2">
+              <input
+                type="checkbox"
+                id="roastbeef-mushrooms"
+                onChange={handleRoastBeefMushrooms}
+              />
+              <label
+                htmlFor="roastbeef-mushrooms"
+                className="p-2 label text-xs"
+              >
+                Add mushrooms @ $3.25
+              </label>
+            </div>
+            <div className="flex px-2">
+              <input
+                type="checkbox"
+                id="roastbeef-onions"
+                onChange={handleRoastBeefOnions}
+              />
+              <label
+                htmlFor="roastbeef-mushrooms"
+                className="p-2 label text-xs"
+              >
+                Add onions @ $1.75
+              </label>
+            </div>
+          </div>
+          <input
+            type="number"
+            id="roastBeef"
+            className="p-2 input input-bordered input-primary"
+            onChange={handleRoastBeef}
+            defaultValue={0}
+          />
+          <p className="p-2">${roastBeef}</p>
+        </div>
+
         {/* desert */}
         <div className="flex flex-col md:flex-row items-center border justify-between">
           <div className="w-full md:w-1/3">
@@ -424,7 +596,6 @@ const MenuForm = () => {
             id="coffee"
             className="p-2 input input-bordered input-primary"
             onChange={handleCoffee}
-            defaultValue="Coffee"
           >
             <option value="">Select</option>
             <option value="Coffee">Coffee/Tea (+$0.50/person)</option>
@@ -445,6 +616,7 @@ const MenuForm = () => {
               type="select"
               id="flatware"
               className="p-2 input input-bordered input-primary w-full"
+              defaultValue="Royal"
               onChange={handleFlatware}
             >
               <option value="">Select</option>
@@ -520,6 +692,15 @@ const MenuForm = () => {
             Total Cost
           </label>
           <p className="p-2 text-xl">${price}</p>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-xs">
+            Total Cost price displayed represents an initial estimate only based on the provided details. Final price will be
+            calculated based on any additional requirements, and will be confirmed prior to request for payment after booking is finalized.
+            Any additional menu requests can be noted in the Service Request Form. 
+            Form.<br />
+            See <Link to="/menu">Menu page</Link> for additional menu options that may be available. <br />
+          </p>
         </div>
         <button
           type="button"
